04006d9eddb5194e55867c0009deb6e82cf1ab6e
Matt Halvorson <halvorson34.mh@gmail.com>
2020-04-19 03:04:33 -0500

    updated Comprehension page with code

--- src/components/Pages/ComprehensionPage/ComprehensionPage.js
+++ src/components/Pages/ComprehensionPage/ComprehensionPage.js
@@ -2,13 +2,54 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class ComprehensionPage extends Component {
+  state = {
+    comprehensionRating: "",
+  };
+
+  onInputChange = (input) => (event) => {
+    this.setState(
+      {
+        [input]: event.target.value,
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  onNextClick = (event) => {
+    this.props.dispatch({
+      type: "SET_COMPREHENSION_RATING",
+      payload: this.state,
+    });
+    this.props.history.push("/support");
+  };
+
   render() {
+    console.log(this.props.store);
     return (
       <div>
-        <p>ComprehensionPage</p>
+        <h1>How well are you understanding the content?</h1>
+        <form>
+          <div id="inputField">
+            <label for="comprehensionInput">Understanding?</label>
+            <br />
+            <input
+              onChange={this.onInputChange("comprehensionRating")}
+              placeholder="1 to 5"
+              type="text"
+              id="comprehensionInput"
+            />
+          </div>
+          <div id="buttonField">
+            <button onClick={this.onNextClick}>NEXT</button>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
-export default connect()(ComprehensionPage);
+const mapStoreToProps = (store) => ({ store });
+
+export default connect(mapStoreToProps)(ComprehensionPage);
